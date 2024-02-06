6908609f19cdc95f057836bd4c9eb6f3434ee9fe
rmocean <radumocean2007@gmail.com>
2024-02-06 16:47:23 -0500

    changes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mybucket-rm-backend"
-    key    = "s3://mybucket-rm-backend"
+    key    = "mybucket-rm-backend/terraform.tfstate"
     region = "us-east-1"
   }
 }
